230337bd6b751a039a3bec458fe22dfcee74bb37
Hasnain848 <muhammadhasnainsiddiqui596@gmail.com>
2024-09-06 08:11:39 +0500

    Update Q4.c

--- Lab03/Q4.c
+++ Lab03/Q4.c
@@ -1,12 +1,13 @@
+
 #include<stdio.h>
 int main ()
 { float a,b,c,x,e,y;
   printf("Enter the car average:");
   scanf("%f", & a);
-  b = a * 1207;   // fuel from A to B and B to A
-  c = b * 118;    // fuel cost from A to B
-  x = b * 123;    // fuel cost from B to A
+float fuel_consumed_one_way = 1207/a;   // fuel from A to B 
+ float Total_fuel_consumed = 2*fuel_consumed_one_way;
+ c = fuel_consumed_one_way * 118;    // fuel cost from A to B
+  x = fuel_consumed_one_way * 123;    // fuel cost from B to A
   e = c + x;      //total fuel cost
-  y = 2*b;        // total fuel consumed
-  printf(" Total fuel cost: %f \n Total Fuel cosumed : %f", e,y);
+  printf(" Total fuel cost: %f \n Total Fuel cosumed : %f", e,Total_fuel_consumed);
 }
