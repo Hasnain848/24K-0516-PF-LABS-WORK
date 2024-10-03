165a85166c0efbe1db1b2756c3e230876b5d1312
Hasnain848 <muhammadhasnainsiddiqui596@gmail.com>
2024-10-03 15:19:20 +0500

    Add files via upload

---
+++ Lab 06/Lab 6 Q.1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{ int a,sum=0;
+while(a!=0)
+{ printf("Enyer the number :\n");
+  scanf("%d",&a);
+  sum=sum+a;
+  
+}
+	
+printf("sum= %d",sum);	
+}
---
+++ Lab 06/Q5 lab.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{int i;
+	for(i=0;i<=6;i++){
+		if(i==0 || i==6)
+		printf("* * * * *\n");
+      else 
+        printf("*       *\n");
+	}
+	
+	
+	
+}
