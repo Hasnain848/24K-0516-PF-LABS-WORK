d4bedc9da389f126930eefd41c977f5eac7e0d6d
Hasnain848 <muhammadhasnainsiddiqui596@gmail.com>
2024-10-09 11:56:26 +0500

    Update  Q.1.cpp

--- Lab 06/Lab 6 Q.1.cpp
+++ Lab 06/Lab 6 Q.1.cpp
@@ -1,3 +1,8 @@
+/*i think the most suitable loop system for taking repeated user input is a while loop because it runs until a specified condition
+becomes false. In the case of user input, you want to continuously prompt for values until the user enters a specific input 
+(in this case, zero). The while loop is ideal because it checks the condition before each iteration and will continue as long 
+as the condition remains true.
+*/
 #include<stdio.h>
 int main ()
 { int a,sum=0;
